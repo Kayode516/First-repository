b8d1d76e67ed495e11aa759ced1e468afb7e60ab
kayode516 <iamkayodeayomidesamuel@gmail.com>
2025-07-27 01:38:45 +0100

    python varaibles

---
+++ calculator.py
@@ -0,0 +1,10 @@
+age = 56
+name = "ademola"
+meter = 8.89
+is_studnet = True
+print("Age:", age)
+print("Name:", name)
+print("Meter:", meter)
+print(is_studnet)
+print(type(meter))
+print(type(is_studnet))
