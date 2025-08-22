c0ac39ff3ff2d0246f5768dff8ead588c85ad34f
kayode516 <iamkayodeayomidesamuel@gmail.com>
2025-08-22 23:53:49 +0100

    Week 2 Assignment Submission

---
+++ week2ass.py
@@ -0,0 +1,23 @@
+# Week two python assignment 
+# My_list  (empty list)
+my_list = []
+# Adding element to list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+# Inserting elements 15 at index 1
+my_list.insert(1, 15)
+print(my_list)
+# extending list with 50, 60 70.
+my_list.extend([50, 60, 70.])
+print(my_list)
+# removing the last on the list
+my_list.pop(-1)
+print(my_list)
+#Sorting my_list
+my_list.sort()
+print(my_list)
+# To print index value of 30
+print(my_list[3])
